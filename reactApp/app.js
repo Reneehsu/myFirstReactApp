d962dd11878d5baed6e1a7267d7da83c769bfb20
git push -u origin <renee8688@gmail.com>
2018-07-26 15:49:50 -0700

    Organizing react code

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,29 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let dummyData = ["Come to SF","Master React"];
+let dummyData = [{ taskText: "Come to SF", completed: false},
+{ taskText: "Master React", completed: true }];
+
+class InputLine extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <input type="text"/>
+        <input type="submit" value="Add todo"/>
+      </div>
+    )
+  }
+}
 
 class Todo extends React.Component {
   constructor(props){
     super(props);
   }
   render(){
-    return(<li><button>X</button>{this.props.task}</li>)
+    return (this.props.task.completed? <li><button>X</button><strike>{this.props.task.taskText}</strike></li> :
+      <li className="blue"><button>X</button>{this.props.task.taskText}</li>)
   }
 }
 
 class TodoList extends React.Component {
   constructor(props){
     super(props);
+    this.state={
+      todos:[],
+    }
   }
   render(){
     return(
       <ul>
-        {dummyData.map( todo => <Todo task={todo}/>)}
+        {this.props.todos.map( todo => <Todo task={todo}/>)}
       </ul>
     )
   }
-
 }
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+class TodoApp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      todos:[],
+    }
+  }
+  componentDidMount(){
+    this.setState({todos:dummyData})
+  }
+  render(){
+    return (
+      <div>
+        <InputLine />
+        <TodoList todos={this.state.todos}/>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
