1a6b3f7034d9f22ae3b75a8b9c33b1555e685d82
git push -u origin <renee8688@gmail.com>
2018-07-26 15:01:13 -0700

    Lets see the list

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!!!!</p>, document.getElementById('root'));
+let dummyData = ["Come to SF","Master React"];
+
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(<li><button>X</button>{this.props.task}</li>)
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <ul>
+        {dummyData.map( todo => <Todo task={todo}/>)}
+      </ul>
+    )
+  }
+
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
